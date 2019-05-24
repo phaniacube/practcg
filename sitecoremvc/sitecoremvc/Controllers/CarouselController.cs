db95f57e023f55ad2152eb4f885734e45f20b2a2
ch <phaneendra.ch@capgemini.com>
2019-05-24 19:42:31 +0530

    fourth commit

---
+++ sitecoremvc/sitecoremvc/Controllers/CarouselController.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Mvc.Presentation;
+using sitecoremvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc;
+using Sitecore.Data.Items;
+
+namespace sitecoremvc.Controllers
+{
+    public class CarouselController : Controller
+    {
+        // GET: Carousel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Carouselimg()
+        {
+            var dataSourceId = RenderingContext.CurrentOrNull.Rendering.DataSource;
+            var dataSource = Sitecore.Context.Database.GetItem(dataSourceId);
+            var kk = dataSource.Children;
+              var viewModel = new CarouselModel
+            {
+               // ImageField = dataSource.Children
+            };
+            return View(viewModel);
+
+           
+        }
+    }
+}
---
+++ sitecoremvc/sitecoremvc/Models/CarouselModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace sitecoremvc.Models
+{
+    public class CarouselModel
+    {
+
+        public ImageField CarouselImage { get; set; }
+        public string Carouseldesc { get; set; }
+        
+        public string Carousellink { get; set; }
+      
+    }
+
+   // public IEnumerable<CarouselModel> PageSubMenu { get; set; }
+} 
