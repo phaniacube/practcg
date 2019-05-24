f6750204d57425d293883fcfeeba1293117bec27
ch <phaneendra.ch@capgemini.com>
2019-05-24 19:22:44 +0530

    second comment

---
+++ phanisample/Helixsample/Src/Feature/Carousel/Code/Controllers/CarouselController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using Sitecore.Mvc.Presentation;
+
+using Sitecore.Mvc;
+using Sitecore.Data.Items;
+
+namespace Helixsample.Feature.Carousel.Controllers
+{
+    
+    public class CarouselController : Controller
+    {
+        // GET: Carousel
+
+        
+        public ActionResult CarouselView()
+
+        {
+
+            var dataSourceId = RenderingContext.CurrentOrNull.Rendering.DataSource;
+            var dataSource = Sitecore.Context.Database.GetItem(dataSourceId);
+            var kk = dataSource.Children;
+           
+            return View(kk);
+           // return View();
+        }
+        [HttpGet]
+        public ActionResult CarouselView1()
+
+        {
+
+            var dataSourceId = RenderingContext.CurrentOrNull.Rendering.DataSource;
+            var dataSource = Sitecore.Context.Database.GetItem(dataSourceId);
+            var kk = dataSource.Children;
+
+            return View(kk);
+            // return View();
+        }
+
+    }
+}
+
